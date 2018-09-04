effcb4589f2f6c22fcc6ca8c8b3864ce7358b0b9
sasayabaku <sasayabaku@yahoo.co.jp>
2018-09-04 17:42:17 +0900

    Add `model_path` in `config.json` #1

--- config/load_config.py
+++ config/load_config.py
@@ -7,8 +7,14 @@ class ConfigGetter(object):
 
     def __init__(self, hostname):
         self.data_path = self.get_data_path(hostname)
+        self.model_path = self.get_model_path(hostname)
 
     def get_data_path(self, hostname):
         f = open("./config/config.json", 'r')
         json_data = json.load(f)
         return json_data[hostname]['data_path']
+
+    def get_model_path(self, hostname):
+        f = open("./config/config.json", 'r')
+        json_data = json.load(f)
+        return json_data[hostname]['model_path']
